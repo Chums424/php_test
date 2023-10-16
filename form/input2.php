7cbd71cf584079331459362871e41103420770d8
Chums424 <chihiro.k.424@gmail.com>
2023-10-16 10:27:40 +0900

    フォームのセキュリティ

--- form/input2.php
+++ form/input2.php
@@ -31,14 +31,15 @@ if(!empty($_POST['btn_submit'])){
 <body>
 
 
-<?PHP if($pageFlag === 1 ) : ?>
-<form method="POST" action="input.php">
+<?php if($pageFlag === 1 ) : ?>
+<form method="POST" action="input2.php">
 氏名
 <?php echo $_POST['your_name'] ;?>
 <br>
 メールアドレス
 <?php echo $_POST['email'] ;?>
 <br>
+<input type="submit" name="back" value="戻る">
 <input type="submit" name="btn_submit" value="送信する">
 <input type="hidden" name="your_name" value="<?php echo $_POST['your_name'] ;?>">
 <input type="hidden" name="email" value="<?php echo $_POST['email'] ;?>">
@@ -46,19 +47,19 @@ if(!empty($_POST['btn_submit'])){
 
 <?php endif; ?>
 
-<?PHP if($pageFlag === 2 ) : ?>
+<?php if($pageFlag === 2 ) : ?>
 送信が完了しました。
 <?php endif; ?>
 
 
-<?PHP if($pageFlag === 0 ) : ?>
+<?php if($pageFlag === 0 ) : ?>
 
-<form method="POST" action="input.php">
+<form method="POST" action="input2.php">
 氏名
-<input type="text" name="your_name">
+<input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])){echo $_POST['your_name'] ;} ?>">
 <br>
 メールアドレス
-<input type="email" name="email">
+<input type="email" name="email" value="<?php if(!empty($_POST['email'])){echo $_POST['email'] ;} ?>">
 <br>
 <input type="submit" name="btn_confirm" value="確認する">
 
